06456054e7d87f0bd2ae89b47b855c58d7ab8c20
seoraroong <karen0705@naver.com>
2024-08-07 15:59:47 +0900

    살려줘

---
+++ myproject/urls.py
@@ -0,0 +1,12 @@
+import subprocess
+import threading
+
+from django.contrib import admin
+from django.urls import path, include
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('travel_recommend.urls')),
+]
