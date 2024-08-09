899e28d3c3c7b15ca13634b1aef5d086d573c689
seoraroong <karen0705@naver.com>
2024-08-09 17:29:25 +0900

    bora hyunna seora merge

--- Jpage/migrations/0003_areabaselist_areacode_categorycode1_categorycode2_and_more.py
+++ Jpage/migrations/0003_areabaselist_areacode_categorycode1_categorycode2_and_more.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("Jpage", "02_initial"),
+        ("Jpage", "0001_initial"),
     ]
 
     operations = [
